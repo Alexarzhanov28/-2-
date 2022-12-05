8f990b00bd8bc3e33fdb18ca7f8740882222fc8f
Alexarzhanov288 <a9137771514@gmail.com>
2022-12-05 21:14:25 +0700

    Решил задачу

---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 7 || a < 1)
+    Console.WriteLine("Введен не верный день недели");
+else if (a == 6 || a == 7)
+    Console.WriteLine("Выходной день");
+else if (a > 0 || a < 7)
+    Console.WriteLine("Будний день");
