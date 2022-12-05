087e679c344ac9015451dc6fe113f7c00d9d537a
Alexarzhanov288 <a9137771514@gmail.com>
2022-12-05 20:33:41 +0700

    Решил задачу

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 100 && a < 1000)
+{
+    int n1 = a % 100;
+    int n2 = n1 / 10;
+    Console.WriteLine($"Вторая цифра этого числа: {n2}");
+}
+else
+    Console.WriteLine("Что то пошло не так");
