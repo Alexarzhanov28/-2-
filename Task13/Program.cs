65e7900e4731e305477d872c93f1f0ecc0653ef5
Alexarzhanov288 <a9137771514@gmail.com>
2022-12-05 20:55:58 +0700

    Задача решена

---
+++ Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13
+Console.Clear();
+Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; a > 1000; i++)
+{
+    a = a / 10;       
+}
+if (a > 100 && a < 1000)
+{
+    int n1 = a % 10;
+    Console.WriteLine($"Третье число: {n1}");
+}
+else if (a < 100)
+    Console.WriteLine("Третьего числа нет");
